fe11c93eccd5d877d191a242868b1e4373f8e76d
yujinju <jinju201118@gmail.com>
2023-05-13 00:15:28 +0900

    jpashop

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,7 +12,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype") //싱글 테이블로 정리했을 때 구분할 수 있도록 음악은 'A', 도서는 'B', 영화는 'M'
-@Getter @Setter
+@Getter
 public class Item {
 
     @Id @GeneratedValue
@@ -24,4 +25,21 @@ public class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //비즈니스 로직 구현
+
+    //stock 증가
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    //stock 감소
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+
 }
